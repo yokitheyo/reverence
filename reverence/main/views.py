52cb4e640a9cac09ff71a8e3d71bc1ec9a2eb245
yokitheyo <razvygs@gmail.com>
2025-02-24 23:16:46 +0300

    created models and catalog

---
+++ reverence/main/views.py
@@ -0,0 +1,52 @@
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from .models import ClothingItem, Category, Size
+
+
+class CatalogView(ListView):
+    model = ClothingItem
+    template_name = "main/product/list.html"
+    context_object_name = "clothing_items"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slugs = self.request.GET.getlist("category")
+        size_names = self.request.GET.getlist("size")
+        min_price = self.request.GET.get("min_price")
+        max_price = self.request.GET.get("max_price")
+
+        if category_slugs:
+            queryset = queryset.filter(category__slug__in=category_slugs)
+
+        if size_names:
+            queryset = queryset.filter(
+                Q(sizes__name__in=size_names)
+                & Q(sizes__clothingitemsize__available=True)
+            )
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        context["sizes"] = Size.objects.all()
+        context["selected_categories"] = self.request.GET.getlist("category")
+        context["selected_sizes"] = self.request.GET.getlist("size")
+        context["min_price"] = self.request.GET.get("min_price", "")
+        context["max_price"] = self.request.GET.get("max_price", "")
+
+        return super().get_context_data(**kwargs)
+
+
+class ClothingItemDetailView(DetailView):
+    model = ClothingItem
+    template_name = "main/product/detail.html"
+    context_object_name = "clothing_item"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
